090233728e84b71793e90220c3a95f682532860b
kevin cloinger <kevin.cloinger@gmail.com>
2025-06-21 14:00:11 -0500

    added circles

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             return
+        dt = clock.tick(60) / 1000
       
 if __name__ == "__main__":
     main()
